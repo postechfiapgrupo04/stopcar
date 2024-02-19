98d20ab437ba3dc9d99e50965beb1e8da11f5c83
brunorosselli <bruno.rosselli@hotmail.com>
2024-02-19 19:30:26 -0300

    [GRUPO-04]Criação do Projeto

---
+++ stopcar/src/main/java/br/com/fiap/stopcar/StopcarApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.stopcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopcarApplication.class, args);
+	}
+
+}
