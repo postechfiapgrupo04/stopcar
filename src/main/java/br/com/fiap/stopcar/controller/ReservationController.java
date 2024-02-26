c76a29e3ba8e2c354c493b82f92fd964111c59e2
Robson Ramos <robinhokcond1@gotmail.com>
2024-02-25 23:10:55 -0300

    Implementando documentação da paaplicação com o swagger

--- src/main/java/br/com/fiap/stopcar/controller/ReservationController.java
+++ src/main/java/br/com/fiap/stopcar/controller/ReservationController.java
@@ -3,6 +3,8 @@ package br.com.fiap.stopcar.controller;
 import br.com.fiap.stopcar.application.dto.ReservationDTO;
 import br.com.fiap.stopcar.application.exceptions.AppException;
 import br.com.fiap.stopcar.service.ReservationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,37 +13,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/reservas")
-public class ReservationController extends AbstractRestController{
-
+@Api(tags = "Reservas", description = "API para operações relacionadas a reservas")
+public class ReservationController {
+    
     private final ReservationService reservationService;
-
+    
     @Autowired
     public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
-
-    // Controller responsável por retornar todas as reservas
+    
+    @ApiOperation("Retorna todas as reservas")
     @GetMapping
     public ResponseEntity<List<ReservationDTO>> getReservations() throws AppException {
         return ResponseEntity.ok(reservationService.getReservation());
     }
-
-    // Controller responsável por retornar uma reserva específica através do id
+    
+    @ApiOperation("Retorna uma reserva específica pelo ID")
     @GetMapping("/{id}")
     public ResponseEntity<ReservationDTO> getReservation(@PathVariable("id") String id) throws AppException {
         return ResponseEntity.ok(reservationService.findById(id));
     }
-
-    // Controller responsável por salvar uma nova reserva
+    
+    @ApiOperation("Salva uma nova reserva")
     @PostMapping
     public ResponseEntity<ReservationDTO> saveReservation(@RequestBody ReservationDTO reservation) {
         return ResponseEntity.ok(reservationService.saveReservation(reservation));
     }
-
-    // Controller responsável por retornar todas as reservas ativas
+    
+    @ApiOperation("Retorna todas as reservas ativas")
     @GetMapping("/ativas")
     public ResponseEntity<List<ReservationDTO>> getActiveReservations() {
         return ResponseEntity.ok(reservationService.getActiveReservations());
     }
-
 }
